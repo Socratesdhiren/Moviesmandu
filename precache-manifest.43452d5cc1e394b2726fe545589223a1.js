35d7c4d1f9b1fd3c936585558748c269967f0393
dhirendra <dhirendra@dhiren.citytech.local>
2020-01-19 23:02:25 +0545

    Updates

---
+++ precache-manifest.43452d5cc1e394b2726fe545589223a1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d88e72b49606f20cbf0347152753778",
+    "url": "/Moviesmandu/index.html"
+  },
+  {
+    "revision": "cefde7b67b7088492c32",
+    "url": "/Moviesmandu/static/css/10.6942973f.chunk.css"
+  },
+  {
+    "revision": "552382191cb8401f2b61",
+    "url": "/Moviesmandu/static/css/5.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "7b96362c0ac899bce468",
+    "url": "/Moviesmandu/static/css/7.6942973f.chunk.css"
+  },
+  {
+    "revision": "083cb9ca36483318a03e",
+    "url": "/Moviesmandu/static/css/8.6942973f.chunk.css"
+  },
+  {
+    "revision": "6295388547be9361bd1e",
+    "url": "/Moviesmandu/static/css/9.6942973f.chunk.css"
+  },
+  {
+    "revision": "3658a6912619c5d854d0",
+    "url": "/Moviesmandu/static/css/main.df609f6e.chunk.css"
+  },
+  {
+    "revision": "b98ecb1a5d958108a919",
+    "url": "/Moviesmandu/static/js/0.a3ef7e11.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/Moviesmandu/static/js/0.a3ef7e11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b75a9ccb410df0bf84e3",
+    "url": "/Moviesmandu/static/js/1.c9026090.chunk.js"
+  },
+  {
+    "revision": "cefde7b67b7088492c32",
+    "url": "/Moviesmandu/static/js/10.a06241d7.chunk.js"
+  },
+  {
+    "revision": "05e32e08982cea9856b7",
+    "url": "/Moviesmandu/static/js/4.1b386403.chunk.js"
+  },
+  {
+    "revision": "552382191cb8401f2b61",
+    "url": "/Moviesmandu/static/js/5.5acb619f.chunk.js"
+  },
+  {
+    "revision": "38d1f7573e3f8e03f12894c14c5352ff",
+    "url": "/Moviesmandu/static/js/5.5acb619f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffa910f6c8efb5d16b68",
+    "url": "/Moviesmandu/static/js/6.af4ec76f.chunk.js"
+  },
+  {
+    "revision": "7b96362c0ac899bce468",
+    "url": "/Moviesmandu/static/js/7.12fb1a9c.chunk.js"
+  },
+  {
+    "revision": "083cb9ca36483318a03e",
+    "url": "/Moviesmandu/static/js/8.312c6478.chunk.js"
+  },
+  {
+    "revision": "6295388547be9361bd1e",
+    "url": "/Moviesmandu/static/js/9.5f21a179.chunk.js"
+  },
+  {
+    "revision": "3658a6912619c5d854d0",
+    "url": "/Moviesmandu/static/js/main.80cf04f1.chunk.js"
+  },
+  {
+    "revision": "3da66390b486941840cf",
+    "url": "/Moviesmandu/static/js/runtime-main.22818194.js"
+  }
+]);
